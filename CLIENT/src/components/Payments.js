e062d3042975e6e918d553b34041edf983a80573
Atuoha <atuohainitiatives@gmail.com>
2021-05-18 00:26:31 +0100

    handling stripe api gateway

--- CLIENT/src/components/Payments.js
+++ CLIENT/src/components/Payments.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-// import StripeCheckout from 'react-stripe-checkout'
+import StripeCheckout from 'react-stripe-checkout'
 
 
 class Payments extends Component{
     render(){
         return(
             <div>
-                {/* <StripeCheckout 
+                { <StripeCheckout 
                     name= "Compaign Manager"
                     description= "Subscription for 5 Email Credits"
                     amount={500} 
@@ -14,8 +14,8 @@ class Payments extends Component{
                     StripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
                  >
                     <button className="btn" id="login-btn">Add Credits <i className="fa fa-money"></i></button>
-                 </StripeCheckout
-                 */}
+                 </StripeCheckout>
+                 }
             </div>
         )
     }
